a00566c72ef5d0f9cc39b62b8f8fdf4faba31938
Luc CADORET <luc.cadoret@gmail.com>
2014-03-09 10:45:45 +0100

    Adding the comparison between the old position and the new position so there is no broadcasting storm

--- app.js
+++ app.js
@@ -5,42 +5,23 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-/* Configuration du répertoire publique */
-/*app.configure(function(){
-    app.use(express.static(__dirname + '/public'));
-});*/
-
 app.get('/',function(req,res){
   res.set('Content-Type', 'text/html'); // 'text/html' => mime type
   res.sendfile(__dirname + '/index.html');
 });
 
-/*app.get('/data/images/:imagename', function(req, res) {
-    res.setHeader('Content-Type', 'image/png');
-    res.sendfile(__dirname + '/' + req.params.imagename);
-});
-
-app.get('/data/javascripts/:filename', function(req, res) {
-    res.setHeader('Content-Type', 'text/javascript');
-    res.sendfile(__dirname + '/' + req.params.filename);
-});
-
-app.get('/data/json/:jsonname', function(req, res) {
-    res.setHeader('Content-Type', 'text/javascript');
-    res.sendfile(__dirname + '/' + req.params.filename);
-});*/
-
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
-  // Mes évènements lorsque le client est connecté !
-	console.log('Un client est connecté !');
-	socket.broadcast.emit('p2Connected'); // le deuxieme argument est facultatif
+
+	console.log('New client');
+	socket.broadcast.emit('p2Connected');s
 	
 	socket.on('move', function (playerData) {
 			socket.broadcast.emit('updatePositions',playerData);
 	});
 });
 
+/* We have to let Heroku choose the port he wants to listen, hence the "process.env.PORT" */
 server.listen(process.env.PORT || 8888);
